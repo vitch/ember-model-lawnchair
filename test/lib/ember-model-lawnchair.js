7f7a9b63388dd4a06a81d48a732fa5327785bb25
Kelvin Luck <git@kelvinluck.com>
2013-09-21 13:27:08 +0800

    Cleaner use of promises to avoid rightward drift...

--- test/lib/ember-model-lawnchair.js
+++ test/lib/ember-model-lawnchair.js
@@ -24,128 +24,127 @@
         createRecord: function(record) {
             var klass = record.constructor;
             return this._initStore(this._getRecordType(klass)).then(function(store) {
-                return new Ember.RSVP.Promise(function(resolve, reject) {
-                    store.save(prepareForSave(klass, record), function(data) {
-                        swapIds(klass, data);
-                        record.load(data[Ember.get(klass, "primaryKey")], data);
-                        record.didCreateRecord();
-                        resolve(record);
-                    });
+                var promise = Ember.Deferred.create();
+                store.save(prepareForSave(klass, record), function(data) {
+                    swapIds(klass, data);
+                    record.load(data[Ember.get(klass, "primaryKey")], data);
+                    record.didCreateRecord();
+                    promise.resolve(record);
                 });
+                return promise;
             });
         },
         saveRecord: function(record) {
             var klass = record.constructor;
             return this._initStore(this._getRecordType(klass)).then(function(store) {
-                return new Ember.RSVP.Promise(function(resolve, reject) {
-                    store.save(prepareForSave(klass, record), function(data) {
-                        swapIds(klass, data);
-                        record.didSaveRecord();
-                        resolve(record);
-                    });
+                var promise = Ember.Deferred.create();
+                store.save(prepareForSave(klass, record), function(data) {
+                    swapIds(klass, data);
+                    record.didSaveRecord();
+                    promise.resolve(record);
                 });
+                return promise;
             });
         },
         deleteRecord: function(record) {
             var klass = record.constructor;
             return this._initStore(this._getRecordType(klass)).then(function(store) {
-                return new Ember.RSVP.Promise(function(resolve, reject) {
-                    store.remove(record.get(Ember.get(klass, "primaryKey")), function() {
-                        record.didDeleteRecord();
-                        resolve(record);
-                    });
+                var promise = Ember.Deferred.create();
+                store.remove(record.get(Ember.get(klass, "primaryKey")), function() {
+                    record.didDeleteRecord();
+                    promise.resolve(record);
                 });
+                return promise;
             });
         },
         find: function(record, id) {
             var klass = record.constructor;
             return this._initStore(this._getRecordType(klass)).then(function(store) {
-                return new Ember.RSVP.Promise(function(resolve, reject) {
-                    store.get(id, function(loadedData) {
-                        if (loadedData) {
-                            swapIds(klass, loadedData);
-                        } else {
-                            loadedData = {};
-                        }
-                        record.load(id, loadedData);
-                        resolve(record);
-                    });
+                var promise = Ember.Deferred.create();
+                store.get(id, function(loadedData) {
+                    if (loadedData) {
+                        swapIds(klass, loadedData);
+                    } else {
+                        loadedData = {};
+                    }
+                    record.load(id, loadedData);
+                    promise.resolve(record);
                 });
+                return promise;
             });
         },
         findQuery: function(klass, records, params) {
             return this._initStore(this._getRecordType(klass)).then(function(store) {
-                return new Ember.RSVP.Promise(function(resolve, reject) {
-                    var paramKeys = Object.keys(params);
-                    store.all(function(loadedData) {
-                        var found = loadedData.filter(function(item) {
-                            var isMatch = true;
-                            paramKeys.forEach(function(key) {
-                                var param = params[key];
-                                var value = item[key];
-                                if (Ember.typeOf(param) === "regexp") {
-                                    if (!value.match(param)) {
-                                        isMatch = false;
-                                    }
-                                } else {
-                                    if (value !== param) {
-                                        isMatch = false;
-                                    }
+                var promise = Ember.Deferred.create();
+                var paramKeys = Object.keys(params);
+                store.all(function(loadedData) {
+                    var found = loadedData.filter(function(item) {
+                        var isMatch = true;
+                        paramKeys.forEach(function(key) {
+                            var param = params[key];
+                            var value = item[key];
+                            if (Ember.typeOf(param) === "regexp") {
+                                if (!value.match(param)) {
+                                    isMatch = false;
                                 }
-                            });
-                            return isMatch;
+                            } else {
+                                if (value !== param) {
+                                    isMatch = false;
+                                }
+                            }
                         });
-                        found.forEach(function(data) {
-                            return swapIds(klass, data);
-                        });
-                        records.load(klass, found);
-                        resolve(records);
+                        return isMatch;
                     });
+                    found.forEach(function(data) {
+                        return swapIds(klass, data);
+                    });
+                    records.load(klass, found);
+                    promise.resolve(records);
                 });
+                return promise;
             });
         },
         findMany: function(klass, records, ids) {
             return this._initStore(this._getRecordType(klass)).then(function(store) {
-                return new Ember.RSVP.Promise(function(resolve, reject) {
-                    store.get(ids, function(data) {
-                        data.forEach(function(item) {
-                            swapIds(klass, item);
-                        });
-                        records.load(klass, data);
-                        resolve(records);
+                var promise = Ember.Deferred.create();
+                store.get(ids, function(data) {
+                    data.forEach(function(item) {
+                        swapIds(klass, item);
                     });
+                    records.load(klass, data);
+                    promise.resolve(records);
                 });
+                return promise;
             });
         },
         findAll: function(klass, records) {
             return this._initStore(this._getRecordType(klass)).then(function(store) {
-                return new Ember.RSVP.Promise(function(resolve, reject) {
-                    store.all(function(data) {
-                        data.forEach(function(item) {
-                            swapIds(klass, item);
-                        });
-                        records.load(klass, data);
-                        resolve(records);
+                var promise = Ember.Deferred.create();
+                store.all(function(data) {
+                    data.forEach(function(item) {
+                        swapIds(klass, item);
                     });
+                    records.load(klass, data);
+                    promise.resolve(records);
                 });
+                return promise;
             });
         },
         _initStore: function(type) {
             var storeName = this.prefix + type;
-            var adapter = this.lawnchairAdapter;
-            return new Ember.RSVP.Promise(function(resolve, reject) {
-                if (!lawn[storeName]) {
-                    new Lawnchair({
-                        name: storeName,
-                        adapter: adapter
-                    }, function(store) {
-                        lawn[storeName] = store;
-                        resolve(store);
-                    });
-                } else {
-                    resolve(lawn[storeName]);
-                }
-            });
+            var promise = Ember.Deferred.create();
+            if (!lawn[storeName]) {
+                new Lawnchair({
+                    name: storeName,
+                    adapter: this.lawnchairAdapter
+                }, function(store) {
+                    lawn[storeName] = store;
+                    promise.resolve(store);
+                });
+            } else {
+                promise.resolve(lawn[storeName]);
+            }
+            return promise;
         },
         _getRecordType: function(klass) {
             var type = Ember.get(klass, "url");
