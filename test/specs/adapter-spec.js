e32e11fb6b698c1fee1b959d9466963d108e3146
Kelvin Luck <git@kelvinluck.com>
2013-09-21 12:46:18 +0800

    Refactoring the tests
    Better use of promises and sharing of fixture data between tests...

--- test/specs/adapter-spec.js
+++ test/specs/adapter-spec.js
@@ -11,6 +11,26 @@
 
   var PostModel;
 
+  var post1Json = {id:1, title: 'Hello world'};
+  var post2Json = {id:2, title:'Goodbye cruel world'};
+  var post3Json = {id:3, title:'Wait a minute - it\'s little Mo!'};
+
+  var createAndSave = function(klass, data) {
+    return function() {
+      return klass.create(data).save();
+    };
+  };
+
+  var clearCache = function(klass) {
+    return function(m) {
+      klass.clearCache();
+      // For some reason this need to be split over three lines??
+      var promise = Ember.Deferred.create();
+      promise.resolve(m);
+      return promise;
+    }
+  };
+
   module('Ember.LawnchairAdapter', {
     setup: function() {
       stop();
@@ -34,31 +54,27 @@
   });
 
   asyncTest('when a record is created with no ID an ID should be automatically set', function() {
-    PostModel
-      .create({title: 'Hello world'})
-      .save().then(function(post) {
+    PostModel.create({title: 'Hello world'}).save()
+      .then(function(post) {
         ok(post.get('id'), 'ID is set');
         start();
       });
   });
 
   asyncTest('when a record is created with an ID that ID should be preserved', function() {
-    var postJson = {id: 1, title: 'Hello world'};
-    PostModel
-      .create(postJson)
-      .save().then(function(post) {
+    PostModel.create(post1Json).save()
+      .then(function(post) {
         ok(post.get('id'), 'ID is set');
-        ok(post.get('id') === postJson.id, 'ID has same value as originally set.');
+        ok(post.get('id') === post1Json.id, 'ID has same value as originally set.');
         start();
       });
   });
 
   asyncTest('when a record is created we should be able to find it by its id', function() {
-    var postJson = {id: 1, title: 'Hello world'};
-    PostModel.create(postJson)
-      .save().then(function(savedPost) {
+    PostModel.create(post1Json).save()
+      .then(function(savedPost) {
 
-        var loadedPost = PostModel.find(postJson.id);
+        var loadedPost = PostModel.find(post1Json.id);
         Ember.loadPromise(loadedPost).then(function() {
           ok(loadedPost, 'a post is found');
           ok(loadedPost === savedPost, 'the loaded post is the same as the saved post');
@@ -68,13 +84,11 @@
   });
 
   asyncTest('when a record is created then deleted from the cache we should be still able to find it by its id', function() {
-    var postJson = {id: 1, title: 'Hello world'};
-    PostModel.create(postJson)
-      .save().then(function(savedPost) {
-        // Clear the cache so that ember-model is forced to load our object from the adapter.
-        PostModel.clearCache();
-
-        var loadedPost = PostModel.find(postJson.id);
+    PostModel.create(post1Json).save()
+      // Clear the cache so that ember-model is forced to load our object from the adapter.
+      .then(clearCache(PostModel))
+      .then(function(savedPost) {
+        var loadedPost = PostModel.find(post1Json.id);
         return Ember.loadPromise(loadedPost).then(function() {
           ok(loadedPost, 'a post is found');
           deepEqual(loadedPost.toJSON(), savedPost.toJSON(), 'the loaded post is the same as the saved post');
@@ -84,11 +98,10 @@
   });
 
   asyncTest('when a record is deleted it should be marked as deleted', function() {
-    var postJson = {id: 1, title: 'Hello world'};
-    PostModel.create(postJson)
-      .save().then(function(savedPost) {
+    PostModel.create(post1Json).save()
+      .then(function(savedPost) {
         savedPost.deleteRecord().then(function() {
-          var loadedPost = PostModel.find(postJson.id);
+          var loadedPost = PostModel.find(post1Json.id);
           ok(loadedPost.isDeleted, 'The model isDeleted');
           start();
         });
@@ -98,29 +111,28 @@
   // The behaviour here is not what I would expect but seems to mirror other ember-model adapters
   // see https://github.com/ebryn/ember-model/issues/248
   asyncTest('when a record is deleted and cache is emptied nothing should be found', function() {
-    var postJson = {id: 1, title: 'Hello world'};
-    PostModel.create(postJson)
-      .save().then(function(savedPost) {
-        savedPost.deleteRecord().then(function() {
-          PostModel.clearCache();
-          var loadedPost = PostModel.find(postJson.id);
-          return Ember.loadPromise(loadedPost).then(
-            function() {
-              // An empty record comes back which isn't what I would expect but the only thing I can think to test against
-              // is that the title comes back empty
-              ok(!loadedPost.get('title'), 'no title for the returned post');
-              start();
-            }
-          );
-        });
+    PostModel.create(post1Json).save()
+      .then(function(savedPost) {
+        return savedPost.deleteRecord()
+          .then(clearCache(PostModel))
+          .then(function() {
+            var loadedPost = PostModel.find(post1Json.id);
+            return Ember.loadPromise(loadedPost).then(
+              function() {
+                // An empty record comes back which isn't what I would expect but the only thing I can think to test against
+                // is that the title comes back empty
+                ok(!loadedPost.get('title'), 'no title for the returned post');
+                start();
+              }
+            );
+          });
       });
   });
 
   asyncTest('when two records are created findAll should return both records', function() {
-    PostModel.create({id:1, title:'Hello world'}).save()
+    PostModel.create(post1Json).save()
+      .then(createAndSave(PostModel, post2Json))
       .then(function() {
-        return PostModel.create({id:2, title:'Goodbye cruel world'}).save();
-      }).then(function() {
         var records = PostModel.find();
         Ember.loadPromise(records).then(function(records) {
           equal(records.get('content').length, 2, 'There are two records');
@@ -130,11 +142,10 @@
   });
 
   asyncTest('when two records are created and one has been removed from cache findAll should return both records', function() {
-    PostModel.create({id:1, title:'Hello world'}).save()
+    PostModel.create(post1Json).save()
+      .then(clearCache(PostModel))
+      .then(createAndSave(PostModel, post2Json))
       .then(function() {
-        PostModel.clearCache();
-        return PostModel.create({id:2, title:'Goodbye cruel world'}).save();
-      }).then(function() {
         var records = PostModel.find();
         Ember.loadPromise(records).then(function(records) {
           equal(records.get('length'), 2, 'There are two records');
@@ -144,12 +155,10 @@
   });
 
   asyncTest('when three records are created we should be able to find two of them with findMany', function() {
-    PostModel.create({id:1, title:'Hello world'}).save()
+    PostModel.create(post1Json).save()
+      .then(createAndSave(PostModel, post2Json))
+      .then(createAndSave(PostModel, post3Json))
       .then(function() {
-        return PostModel.create({id:2, title:'Goodbye cruel world'}).save();
-      }).then(function() {
-        return PostModel.create({id:3, title:'Wait a minute - it\'s little Mo!'}).save();
-      }).then(function() {
         var records = PostModel.find([2,3]);
         Ember.loadPromise(records).then(function(records) {
           equal(records.get('length'), 2, 'Two records are found');
@@ -161,25 +170,23 @@
   });
 
   asyncTest('when three records are created then the cache is cleared we should still be able to find two of them with findMany', function() {
-    PostModel.create({id:1, title:'Hello world'}).save()
+    PostModel.create(post1Json).save()
+      .then(createAndSave(PostModel, post2Json))
+      .then(createAndSave(PostModel, post3Json))
+      .then(clearCache(PostModel))
       .then(function() {
-        return PostModel.create({id:2, title:'Goodbye cruel world'}).save();
-      }).then(function() {
-        return PostModel.create({id:3, title:'Wait a minute - it\'s little Mo!'}).save();
-      }).then(function() {
-        PostModel.clearCache();
         var records = PostModel.find([3,1]);
-        Ember.loadPromise(records).then(function(records) {
+        return Ember.loadPromise(records).then(function(records) {
           equal(records.get('length'), 2, 'Two records are found');
-          equal(records.get('firstObject.id'), 3, 'The first item has an ID of 1');
-          equal(records.get('lastObject.id'), 1, 'The lastitem has an ID of 3');
+          equal(records.get('firstObject.id'), 3, 'The first item has an ID of 3');
+          equal(records.get('lastObject.id'), 1, 'The last item has an ID of 1');
           start();
         });
       });
   });
 
   asyncTest('when a record is created, saved and then edited and saved the adapter should save the new record', function() {
-    PostModel.create({id:1, title:'Hello world'}).save()
+    PostModel.create(post1Json).save()
       .then(function(model) {
          model.set('title', 'Bonjour monde!').save().then(function() {
            Ember.loadPromise(PostModel.find(1)).then(function(loadedPost) {
@@ -190,6 +197,6 @@
       });
   });
 
-  // TODO: Test Model.find(<Object), Model.fetch(), Model.fetch(id), Model.fetch(<Object>), Model.load([]), Relationships
+  // TODO: Test Model.find(<Object>), Model.fetch(), Model.fetch(id), Model.fetch(<Object>), Model.load([]), Relationships
 
 })();
